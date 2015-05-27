35444c397e2b60b3d0eefa81f8132f8d3d4b814e
Claude Akakpo <akdclaude@yahoo.com>
2015-05-27 02:25:43 +0000

    Added plan-id to the users table to establish the relationship

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,4 @@
 class Plan < ActiveRecord::Base
-  
+  # This code " has_many :users" means: Plan has many users
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # :The below statement can be interpreted as: User has a plan
+  belongs_to :plan
 end
